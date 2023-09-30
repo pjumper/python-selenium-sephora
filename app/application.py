20f249fd96985e92865f5a19408b9aa404b939da
Chris <user@mac-mini.attlocal.net>
2023-09-29 22:58:15 -0700

    Initial Update

--- app/application.py
+++ app/application.py
@@ -1,6 +1,9 @@
 from pages.main_page import MainPage
+from pages.header import Header
 
 
 class Application:
     def __init__(self, driver):
         self.driver = driver
+        self.main_page = MainPage(self.driver)
+        self.header = Header(self.driver)
---
+++ pages/main_page.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import Page
+from time import sleep
+
+class MainPage(Page):
+
+
+
+    def open_home(self):
+        self.open_url('https://www.sephora.com/')
+        sleep(5)
